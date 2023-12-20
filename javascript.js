0c746d706757e13703aa8cef1b5d913d64c1dedb
pmmachadov <pmmachadov@gmail.com>
2023-12-20 02:00:25 +0100

    Show many pokemons and css style with one getElementById

--- javascript.js
+++ javascript.js
@@ -1,74 +1,66 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // const url = "https://pokeapi.co/api/v2/pokemon/";
-  const url = "https://pokeapi.co/api/v2/ability/?limit=20&offset=20";
-  let pokemon = {};
+  const numberOfPokemon = 10;
+  const url = `https://pokeapi.co/api/v2/pokemon/?limit=${numberOfPokemon}`;
+  let pokemons = [];
 
   fetch(url)
     .then(async (response) => {
       if (response.status === 200) {
-        pokemon = await response.json();
+        const data = await response.json();
+        pokemons = data.results;
 
-        const image = pokemon.sprites.front_default;
-        const name = pokemon.name;
-        const pokemonId = pokemon.id;
-        const type = pokemon.types[0].type.name;
-        const ability = pokemon.abilities[0].ability.name;
-        const move = pokemon.moves[0].move.name;
-        const weight = pokemon.weight;
-        const move_ailment = pokemon.moves[0].version_group_details[0].move_learn_method.name;
-        const nature = pokemon.nature;
-        const id = pokemon.id;
-        const types = pokemon.types.map(type => type.type.name);
-        const moves = pokemon.moves.slice(0, 4).map(move => move.move.name);
+        pokemons.forEach(async (pokemonData) => {
+          const pokemon = await fetchPokemonDetails(pokemonData.url);
 
+          const image = pokemon.sprites?.front_default || "N/A";
+          const name = pokemon.name || "N/A";
+          const pokemonId = pokemon.id || "N/A";
+          const weight = pokemon.weight || "N/A";
+          const types = pokemon.types?.map(type => type.type.name).join(', ') || "N/A";
+          const moves = pokemon.moves?.slice(0, 4).map(move => move.move.name).join(', ') || "N/A";
+          const ability = pokemon.abilities?.length > 0 ? pokemon.abilities[0].ability.name : "N/A";
+          const move_ailment = pokemon.moves?.length > 0 && pokemon.moves[0].version_group_details.length > 0
+            ? pokemon.moves[0].version_group_details[0].move_learn_method.name
+            : "N/A";
 
+          // Log information to the console
+          console.log('------------------------');
+          console.log(`Name: ${name}`);
+          console.log(`ID: ${pokemonId}`);
+          console.log(`Weight: ${weight}`);
+          console.log(`Types: ${types}`);
+          console.log(`Moves: ${moves}`);
+          console.log(`Ability: ${ability}`);
+          console.log(`Move Ailment: ${move_ailment}`);
+          console.log('------------------------');
 
-
-        // Mostrar el nombre y tipo en el HTML
-        document.getElementById("image").src = image;
-        document.getElementById("pokemon_name").textContent = "Name: " + name;
-        document.getElementById("pokemon_id").textContent = "ID: " + pokemonId;
-        document.getElementById("type").textContent = "Type: " + type;
-        document.getElementById("ability").textContent = "Ability: " + ability;
-        document.getElementById("move").textContent = "Move: " + move;
-        document.getElementById("weight").textContent = "Weight: " + weight;
-        document.getElementById("move_ailment").textContent = "Move Ailment: " + move_ailment;
-        document.getElementById("nature").textContent = "Nature: " + nature;
-        document.getElementById("id").textContent = "ID: " + id;
-        document.getElementById("types").textContent = "Types: " + types;
-        document.getElementById("moves").textContent = "Moves: " + moves;
-
-
-
-
-
-
-
-
-
-
-        // Mostrar más datos en la consola
-        console.log("Image:", image);
-        console.log("Name:", name);
-        console.log("Type:", type);
-        console.log("Ability:", ability);
-        console.log("Move:", move);
-        console.log("Weight:", weight);
-        console.log("Move Ailment:", move_ailment);
-        console.log("Nature:", nature);
-        console.log("ID:", id);
-        console.log("Types:", types);
-        console.log("Moves:", moves);
-
-
-
-
-
+          // Display information in the HTML
+          const pokemonContainer = document.getElementById("pokemon_container");
+          const pokemonElement = document.createElement("div");
+          pokemonElement.innerHTML = `
+            <h3>${name}</h3>
+            <img src="${image}" alt="${name}">
+              <p>ID: ${pokemonId}</p>
+              <p>Weight: ${weight}</p>
+              <p>Types: ${types}</p>
+              <p>Moves: ${moves}</p>
+              <p>Ability: ${ability}</p>
+              <p>Move Ailment: ${move_ailment}</p>
+              <hr>
+                `;
+          pokemonContainer.appendChild(pokemonElement);
+        });
       } else {
-        throw new Error(response.status);
+        console.error("Failed to fetch data");
       }
     })
     .catch((error) => {
-      console.log(error);
+      console.error("Error:", error);
     });
+
+  async function fetchPokemonDetails(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
 });
