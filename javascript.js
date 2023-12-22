caee4cfbc3eb2ab89025a817d03857c214c7ce8a
pmmachadov <pmmachadov@gmail.com>
2023-12-22 01:07:58 +0100

    CSS modified

--- javascript.js
+++ javascript.js
@@ -1,88 +1,81 @@
-
-// define buttons, input line
 let btn = document.getElementsByTagName("button")[0];
 let input = document.getElementById("userinput");
 let rand_btn = document.getElementById("random");
 const pokemon_html = document.querySelector('.pokemon')
 
-
 const SearchPokemon = (api_obj) => {
-
-  const { url, type, name } = api_obj //destructured object
-  const api_url = `${url}${type}/${name}` //URL string
+  const { url, type, name } = api_obj;
+  const api_url = `${url}${type}/${name}`;
 
   fetch(api_url)
     .then((raw_data) => raw_data.json())
     .then((data) => changeHtml(data))
-    .catch((err) => { //if some error happens, it shows the following message
+    .catch((err) => {
       pokemon_html.innerHTML =
-        `<h1> Some Error Occured.. Please revise your code! </h1>`;
-    })
+        `<h1> Some Error Occurred.. Please revise your code! </h1>`;
+    });
 
   const changeHtml = (data) => {
-
     let new_stats = [];
     const get_stats = () => {
-      for (i = 0; i < data.stats.length; i++) {
+      for (let i = 0; i < data.stats.length; i++) {
         new_stats += [`<p> ${data.stats[i].stat.name} has base-stat of <span class="out">${data.stats[i].base_stat}</span> </p>`];
       }
-      return new_stats
-    }
-    const got_stats = get_stats() //to have all stats as one variable
+      return new_stats;
+    };
+    const got_stats = get_stats();
 
     const newHtml = `
-		<div class = "details" align="center">
-			<h1 class= "name" > ${data.name} </h1>
-			<img src= "${data.sprites.other.dream_world.front_default ? data.sprites.other.dream_world.front_default : data.sprites.front_default ? data.sprites.front_default :
-        "https://thumbs.dreamstime.com/b/no-pokemon-here-sign-riga-latvia-july-restricted-area-over-white-background-go-very-popular-virtual-74549871.jpg"} " /> 
-			<h3> weight: <span class="out">${data.weight} hg </span> </h3>
-			<h3> height: <span class="out">${data.height} dm</span> </h3>
-			<h3> type: <span class="out">${data.types[0].type.name} </span> </h3>
-		</div>
-		<div class= "stats">
-				<h3>${data.name}'s stats: </h3>
-				${got_stats}
-		</div>`
-    pokemon_html.innerHTML = newHtml //add it into html
-    input.value = ""; //to reset the input line
-  }
+	<div class="details" align="center">
+		<h1 class="name">${data.name}</h1>
+		<img src="${data.sprites.other.dream_world.front_default || data.sprites.front_default || "https://thumbs.dreamstime.com/b/no-pokemon-here-sign-riga-latvia-july-restricted-area-over-white-background-go-very-popular-virtual-74549871.jpg"}" /> 
+		<h3>weight: <span class="out">${data.weight} hg</span></h3>
+		<h3>height: <span class="out">${data.height} dm</span></h3>
+		<h3>type: <span class="out">${data.types[0]?.type.name || ""}</span></h3>
+	</div>
+	<div class="stats">
+		<h3>${data.name}'s stats: </h3>
+		${got_stats}
+	</div>`;
 
-}
+    pokemon_html.innerHTML = newHtml;
+    input.value = "";
+  };
+};
 
-function inputLength() { //checks if the input line input is not empty
+function inputLength() {
   return input.value.length;
 }
 
-function MakeUrl(value) { //creates the URL using "value"
+function MakeUrl(value) {
   const api_obj = {
     url: "https://pokeapi.co/api/v2/",
     type: "pokemon",
     name: value,
-  }
+  };
   return api_obj;
 }
 
-function getRandomInt(min, max) { //creates random integer
+function getRandomInt(min, max) {
   let rand_int = Math.floor(Math.random() * (max - min) + min);
   console.log(rand_int);
   return rand_int;
 }
 
 function Randomize(event) {
-  const search_value = getRandomInt(1, 897); //gets random integer between min and max of Pokemon IDs
-  SearchPokemon(MakeUrl(search_value)); //uses gotten integer as Pokemon ID and search
+  const search_value = getRandomInt(1, 897);
+  SearchPokemon(MakeUrl(search_value));
 }
-
 
 function SearchAfterClick(event) {
   if (inputLength() > 0) {
-    SearchPokemon(MakeUrl(input.value)); //search Pokemon by using inputted value
+    SearchPokemon(MakeUrl(input.value));
   }
 }
 
 function SearchAfterKeypress(event) {
-  if (inputLength() > 0 && event.keyCode === 13) { //checks the Enter keyboard command
-    SearchPokemon(MakeUrl(input.value)); //search Pokemon by using inputted value
+  if (inputLength() > 0 && event.keyCode === 13) {
+    SearchPokemon(MakeUrl(input.value));
   }
 }
 btn.addEventListener("click", SearchAfterClick);
