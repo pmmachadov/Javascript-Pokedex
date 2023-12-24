2eb85f7a418f997a800ab52c5319ad5bb578565f
pmmachadov <pmmachadov@gmail.com>
2023-12-24 01:00:20 +0100

    Cards and searchbar together

--- javascript.js
+++ javascript.js
@@ -28,7 +28,7 @@ const SearchPokemon = (api_obj) => {
     const newHtml = `
 	<div class="details" align="center">
 		<h1 class="name">${data.name}</h1>
-		<img src="${data.sprites.other.dream_world.front_default || data.sprites.front_default || "https://thumbs.dreamstime.com/b/no-pokemon-here-sign-riga-latvia-july-restricted-area-over-white-background-go-very-popular-virtual-74549871.jpg"}" /> 
+		<img src="${data.sprites.other.dream_world.front_default || data.sprites.front_default}" /> 
 		<h3>weight: <span class="out">${data.weight} hg</span></h3>
 		<h3>height: <span class="out">${data.height} dm</span></h3>
 		<h3>type: <span class="out">${data.types[0]?.type.name || ""}</span></h3>
@@ -81,3 +81,144 @@ function SearchAfterKeypress(event) {
 btn.addEventListener("click", SearchAfterClick);
 input.addEventListener("keypress", SearchAfterKeypress);
 rand_btn.addEventListener("click", Randomize);
+
+
+// Cards
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const numberOfPokemon = 10;
+  const url = `https://pokeapi.co/api/v2/pokemon/?limit=${numberOfPokemon}`;
+  let pokemons = {};
+
+  try {
+    const response = await fetch(url);
+    if (response.status === 200) {
+      const data = await response.json();
+
+      await Promise.all(data.results.map(async (pokemonData) => {
+        const pokemon = await fetchPokemonDetails(pokemonData.url);
+        await processPokemon(pokemon);
+      }));
+    } else {
+      console.error("Failed to fetch data");
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+
+  async function fetchPokemonDetails(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
+
+  async function processPokemon(pokemon) {
+    try {
+      const image = getImage(pokemon);
+      const name = getName(pokemon);
+      const pokemonId = getPokemonId(pokemon);
+      const weight = getWeight(pokemon);
+      const types = getTypes(pokemon);
+      const moves = getMoves(pokemon);
+      const ability = getAbility(pokemon);
+      const moveAilment = getMoveAilment(pokemon);
+
+      const pokemonInfo = {
+        name: name,
+        image: image,
+        weight: weight,
+        types: types,
+        moves: moves,
+        ability: ability,
+        moveAilment: moveAilment
+      };
+
+      pokemons[pokemonId] = pokemonInfo;
+
+      const pokemonContainer = document.getElementById("pokemon_container");
+      const pokemonCard = document.createElement("div");
+      pokemonCard.className = "card";
+      pokemonCard.innerHTML = `
+      <h3>${name}</h3>
+      <div class="pokemon-image">
+        <img data-src="${image}" alt="${name}" loading="lazy">
+      </div>
+      <div class="card-body">
+        <p><h4>ID:</h4> ${pokemonId}</p>
+        <p><h4>Weight:</h4> ${weight}</p>
+        <p><h4>Types:</h4> ${types}</p>
+        <p><h4>Moves:</h4> ${moves}</p>
+        <p><h4>Ability:</h4> ${ability}</p>
+        <p><h4>Move Ailment:</h4> ${moveAilment}</p>
+      </div>
+    `;
+      pokemonContainer.appendChild(pokemonCard);
+    } catch (error) {
+      console.error("Error processing Pokemon:", error);
+    }
+  }
+
+  function lazyLoadImages() {
+    const images = document.querySelectorAll('img[data-src]');
+    images.forEach(img => {
+      img.src = img.dataset.src;
+      img.removeAttribute('data-src');
+    });
+  }
+
+  if ('IntersectionObserver' in window) {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          const lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.removeAttribute('data-src');
+          observer.unobserve(lazyImage);
+        }
+      });
+    });
+
+    const lazyImages = document.querySelectorAll('img[data-src]');
+    lazyImages.forEach(img => {
+      observer.observe(img);
+    });
+  } else {
+    // Elimina la siguiente línea si decides no usar lazyLoadImages
+    lazyLoadImages();
+  }
+
+  function getImage(pokemon) {
+    return pokemon.sprites?.other?.dream_world?.front_default || "N/A";
+
+  }
+
+  function getName(pokemon) {
+    return pokemon.name || "N/A";
+  }
+
+  function getPokemonId(pokemon) {
+    return pokemon.id || "N/A";
+  }
+
+  function getWeight(pokemon) {
+    return pokemon.weight || "N/A";
+  }
+
+  function getTypes(pokemon) {
+    return pokemon.types?.map(type => type.type.name).join(', ') || "N/A";
+  }
+
+  function getMoves(pokemon) {
+    return pokemon.moves?.slice(0, 4).map(move => move.move.name).join(', ') || "N/A";
+  }
+
+  function getAbility(pokemon) {
+    return pokemon.abilities?.length > 0 ? pokemon.abilities[0].ability.name : "N/A";
+  }
+
+  function getMoveAilment(pokemon) {
+    return pokemon.moves?.length > 0 && pokemon.moves[0].version_group_details.length > 0
+      ? pokemon.moves[0].version_group_details[0].move_learn_method.name
+      : "N/A";
+  }
+});
