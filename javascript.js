db58ab02792890d835191edb65ff09080b191733
pmmachadov <pmmachadov@gmail.com>
2023-12-26 20:56:23 +0100

    Random card organization

--- javascript.js
+++ javascript.js
@@ -1,90 +1,84 @@
-let btn = document.getElementsByTagName("button")[0];
-let input = document.getElementById("userinput");
-let rand_btn = document.getElementById("random");
-const pokemon_html = document.querySelector('.pokemon')
+const btn = document.getElementsByTagName("button")[0];
+const input = document.getElementById("userinput");
+const randBtn = document.getElementById("random");
+const pokemonHtml = document.querySelector('.pokemon');
 
 const pokemons = {};
 
-const SearchPokemon = (api_obj) => {
-  const { url, type, name } = api_obj;
-  const api_url = `${url}${type}/${name}`;
+const searchPokemon = async (apiObj) => {
+  const { url, type, name } = apiObj;
+  const apiUrl = `${url}${type}/${name}`;
 
-  fetch(api_url)
-    .then((raw_data) => raw_data.json())
-    .then((data) => changeHtml(data))
-    .catch((err) => {
-      pokemon_html.innerHTML =
-        `<h1> Some Error Occurred.. Please revise your code! </h1>`;
-    });
+  try {
+    const rawData = await fetch(apiUrl);
+    const data = await rawData.json();
+    changeHtml(data);
+  } catch (err) {
+    handleError();
+  }
 
-  const changeHtml = (data) => {
-    let new_stats = [];
-    const get_stats = () => {
-      for (let i = 0; i < data.stats.length; i++) {
-        new_stats += [`<p> ${data.stats[i].stat.name} has base-stat of <span class="out">${data.stats[i].base_stat}</span> </p>`];
-      }
-      return new_stats;
-    };
-    const got_stats = get_stats();
+  function changeHtml(data) {
+    const newStats = data.stats.map(stat => `<p>${stat.stat.name} <span class="out">${stat.base_stat}</span></p>`);
 
     const newHtml = `
-	<div class="details" align="center">
-		<h1 class="name">${data.name}</h1>
-		<img src="${data.sprites.other.dream_world.front_default || data.sprites.front_default}" /> 
-		<h3>weight: <span class="out">${data.weight} hg</span></h3>
-		<h3>height: <span class="out">${data.height} dm</span></h3>
-		<h3>type: <span class="out">${data.types[0]?.type.name || ""}</span></h3>
-	</div>
-	<div class="stats">
-		<h3>${data.name}'s stats: </h3>
-		${got_stats}
-	</div>`;
+      <div class="details" align="center">
+        <h1 class="name">${data.name}</h1>
+        <img src="${data.sprites.other.dream_world.front_default || data.sprites.front_default}" /> 
+        <table>
+          <tr><th>Stat</th><th>Value</th></tr>
+          <tr><td>Weight</td><td><span class="out">${data.weight} hg</span></td></tr>
+          <tr><td>Types</td><td><span class="out">${data.types[0].type.name}</span></td></tr>
+          <tr><td>Moves</td><td><span class="out">${data.moves[0].move.name}</span></td></tr>
+          <tr><td>Ability</td><td><span class="out">${data.abilities[0].ability.name}</span></td></tr>
+          <tr><td>Move Ailment</td><td><span class="out">${data.moves[0].version_group_details[0].move_learn_method.name}</span></td></tr>
+        </table>
+      </div>
+     `;
 
-    pokemon_html.innerHTML = newHtml;
+    pokemonHtml.innerHTML = newHtml;
     input.value = "";
-  };
+  }
 };
 
+// Helper Functions
 function inputLength() {
   return input.value.length;
 }
 
-function MakeUrl(value) {
-  const api_obj = {
+function makeUrl(value) {
+  return {
     url: "https://pokeapi.co/api/v2/",
     type: "pokemon",
     name: value,
   };
-  return api_obj;
 }
 
 function getRandomInt(min, max) {
-  let rand_int = Math.floor(Math.random() * (max - min) + min);
-  return rand_int;
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
-function Randomize(event) {
-  const search_value = getRandomInt(1, 897);
-  SearchPokemon(MakeUrl(search_value));
+function randomize(event) {
+  const searchValue = getRandomInt(1, 897);
+  searchPokemon(makeUrl(searchValue));
 }
 
-function SearchAfterClick(event) {
+function searchAfterClick(event) {
   if (inputLength() > 0) {
-    SearchPokemon(MakeUrl(input.value));
+    searchPokemon(makeUrl(input.value));
   }
 }
 
-function SearchAfterKeypress(event) {
+function searchAfterKeypress(event) {
   if (inputLength() > 0 && event.keyCode === 13) {
-    SearchPokemon(MakeUrl(input.value));
+    searchPokemon(makeUrl(input.value));
   }
 }
-btn.addEventListener("click", SearchAfterClick);
-input.addEventListener("keypress", SearchAfterKeypress);
-rand_btn.addEventListener("click", Randomize);
+
+btn.addEventListener("click", searchAfterClick);
+input.addEventListener("keypress", searchAfterKeypress);
+randBtn.addEventListener("click", randomize);
 
 // Cards
-
 document.addEventListener("DOMContentLoaded", async () => {
   const pokemonContainer = document.getElementById("pokemon_container");
   const loadMoreBtn = document.getElementById("load_more");
@@ -93,6 +87,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   async function fetchAndProcessPokemonBatch() {
     const url = `https://pokeapi.co/api/v2/pokemon/?limit=${numberOfPokemonPerPage}&offset=${offset}`;
+
     try {
       const response = await fetch(url);
       if (response.status === 200) {
@@ -128,7 +123,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     return data;
   }
 
-  async function processPokemon(pokemon) {
+  function processPokemon(pokemon) {
     const image = getImage(pokemon);
     const name = getName(pokemon);
     const pokemonId = getPokemonId(pokemon);
@@ -153,22 +148,21 @@ document.addEventListener("DOMContentLoaded", async () => {
     const pokemonCard = document.createElement("div");
     pokemonCard.className = "card";
     pokemonCard.innerHTML = `
-        <h3>${name}</h3>
-        <div class="pokemon-image">
-            <img data-src="${image}" alt="${name}" loading="lazy">
-        </div>
-        <div class="card-body">
-            <p><strong>ID:</strong> ${pokemonId}</p>
-            <p><strong>Weight:</strong> ${weight}</p>
-            <p><strong>Types:</strong> ${types}</p>
-            <p><strong>Moves:</strong> ${moves}</p>
-            <p><strong>Ability:</strong> ${ability}</p>
-            <p><strong>Move Ailment:</strong> ${moveAilment}</p>
-        </div>
+      <h3>${name}</h3>
+      <div class="pokemon-image">
+        <img data-src="${image}" alt="${name}" loading="lazy">
+      </div>
+      <div class="card-body">
+        <p><strong>ID:</strong> ${pokemonId}</p>
+        <p><strong>Weight:</strong> ${weight}</p>
+        <p><strong>Types:</strong> ${types}</p>
+        <p><strong>Moves:</strong> ${moves}</p>
+        <p><strong>Ability:</strong> ${ability}</p>
+        <p><strong>Move Ailment:</strong> ${moveAilment}</p>
+      </div>
     `;
     pokemonContainer.appendChild(pokemonCard);
   }
-
 
   function lazyLoadImages() {
     const images = document.querySelectorAll('img[data-src]');
